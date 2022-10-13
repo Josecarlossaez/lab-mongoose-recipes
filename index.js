d0cf79b5a82b4590f798b38cb947ab062b4e2b25
Jose Carlos Sáez Fernández <jcsaezfernandez@gmail.com>
2022-10-13 19:04:05 +0200

    iteration 2 finished

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require('./models/Recipe.model.js');
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require('./data.json');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -13,10 +13,34 @@ mongoose
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    //return Recipe.deleteMany()
+
+   return Recipe.create({
+    
+      "title": "Chocolate Chip Cookies",
+      "level": "Amateur Chef",
+      "ingredients": [
+        "1/2 cup light brown sugar",
+        "1 large egg",
+        "2 tablespoons milk",
+        "1 1/4 teaspoons vanilla extract",
+        "2 cups semisweet chocolate chips"
+      ],
+      "cuisine": "French",
+      "dishType": "dessert",
+      "image": "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F4398987.jpg&w=596&h=399.32000000000005&c=sc&poi=face&q=85",
+      "duration": 30,
+      "creator": "Chef Jennifer",
+     
+    
+    })
   })
-  .then(() => {
+  .then((response) => {
+    console.log("receta añadida");
+    console.log(response);
     // Run your code here, after you have insured that the connection was made
+
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
